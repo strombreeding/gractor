118e4fa7d9ca23595d64262075fe7e2f08cd93a3
이진희 <speaker1403@gmail.com>
2023-03-23 16:50:26 +0900

    mongoURL

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,8 +12,7 @@ import { LocationModule } from './location/location.module';
     PublicApiModule,
     ConfigModule.forRoot(),
     MongooseModule.forRoot(
-      process.env.MONGODB_URL ||
-        'mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb',
+      'mongodb://mongodb:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.2',
     ),
   ],
   controllers: [AppController],
