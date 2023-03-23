c0279d6ae95c9339dce99f4c88787c4fad561b7c
이진희 <speaker1403@gmail.com>
2023-03-23 17:26:42 +0900

    complate connection mongoDB and build db

--- src/gps/gps.controller.ts
+++ src/gps/gps.controller.ts
@@ -1,18 +1,27 @@
 import { Get, Param, Query } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { GpsDto } from './dto/get.dto';
 import { GpsService } from './gps.service';
 
 @Controller('gps')
+@ApiTags('좌표값 구하는 곳 ')
 export class GpsController {
   constructor(private gpsService: GpsService) {}
 
-  // @Get('/insert')
-  // async insertData() {
-  //   const result = await this.gpsService.insertData();
-  //   return result;
-  // }
+  @Get('/insert')
+  async insertData() {
+    const result = await this.gpsService.insertData();
+    return result;
+  }
 
+  @ApiOperation({
+    summary: '지역입력시 좌표값 반환',
+    description: '조건으로 검색하여 좌표값 반환',
+  })
+  @ApiCreatedResponse({
+    status: 200,
+  })
   @Get('/')
   async gps(@Query() query: GpsDto) {
     const { Do, si, vilage } = query;
