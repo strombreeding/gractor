b11daddb07ef02d513f79eaa7525ff1ca1246023
DESKTOP-5SC99UJ\philo <philomon@naver.com>
2023-04-03 19:25:37 +0900

    자동 배포 및 무중단 배포 완료

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,6 +4,7 @@ import axios from 'axios';
 @Injectable()
 export class AppService {
   getHello(): Date {
-    return new Date();
+    const KoreaHour = Date.now() + 9 * 60 * 60 * 1000;
+    return new Date(KoreaHour);
   }
 }
