3cf57dcdbdae66eda042e7a46e323ca0575e34d3
DESKTOP-5SC99UJ\philo <philomon@naver.com>
2023-04-03 19:48:57 +0900

    docker auto deploy 완료!!

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): Date {
+  getHello(): string {
     return this.appService.getHello();
   }
 }
